40dd779bf983c61d8e6b4d67d19d408beecc3224
Audi Bramastya <audibram14@gmail.com>
2023-09-03 00:12:05 +0700

    implements individual product screen

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    &copy; 2023 Bengs Store, Inc. All rights reserved.
+                    &copy; 2023 Bengs Store by Audi Bramastya Oroh, Inc. All rights reserved.
                 </p>
             </div>
         </footer>
